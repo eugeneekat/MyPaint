63ce5391c0a385d909dab11af7de103efa90f24c
Eugene <ekaterinenko88@gmail.com>
2017-02-10 15:55:58 +0200

    Complete

--- MyPaint/Form1.cs
+++ MyPaint/Form1.cs
@@ -1,13 +1,9 @@
 ﻿using MyPaint.Creator;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
